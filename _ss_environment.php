ab31068b99a26f2b0170f2e94b931fd785a03168
GrayMediaLtd <jesse@graymedialtd.com>
2019-03-29 15:40:24 +1300

    added environment file

---
+++ _ss_environment.php
@@ -0,0 +1,11 @@
+<?php
+
+define('SS_ENVIRONMENT_TYPE', 'live');
+
+define('SS_DATABASE_SERVER', '127.0.0.1');
+define('SS_DATABASE_USERNAME', 'snickelmysql');
+define('SS_DATABASE_PASSWORD', 'ySa9w$66ySa9w$66');
+define('SS_DATABASE_NAME', 'admin_snickel');
+
+define('SS_DEFAULT_ADMIN_USERNAME', 'nzd');
+define('SS_DEFAULT_ADMIN_PASSWORD', 'logmeintoaservercmsplease007*');
