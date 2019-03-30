93c58d5f57a62703df020433b103c86b8eac76a3
GrayMediaLtd <jesse@graymedialtd.com>
2019-03-30 17:29:04 +1300

    updated environment file

--- _ss_environment.php
+++ _ss_environment.php
@@ -4,7 +4,7 @@ define('SS_ENVIRONMENT_TYPE', 'live');
 
 define('SS_DATABASE_SERVER', '127.0.0.1');
 define('SS_DATABASE_USERNAME', 'forge');
-define('SS_DATABASE_PASSWORD', 'JiPLiJXpUKUCl39kouTt');
+define('SS_DATABASE_PASSWORD', 'L1gUgVBIIiRDJzBcmefz');
 define('SS_DATABASE_NAME', 'snickellane');
 
 define('SS_DEFAULT_ADMIN_USERNAME', 'nzd');
