c6f053c6a7bbcd06a7f94a4c3da82094909f98f6
GrayMediaLtd <jesse@graymedialtd.com>
2019-03-30 23:47:46 +1300

    updated db password

--- _ss_environment.php
+++ _ss_environment.php
@@ -5,7 +5,7 @@ define('SS_ENVIRONMENT_TYPE', 'live');
 define('SS_DATABASE_SERVER', '127.0.0.1');
 define('SS_DATABASE_USERNAME', 'forge');
 define('SS_DATABASE_PASSWORD', 'L1gUgVBIIiRDJzBcmefz');
-define('SS_DATABASE_NAME', 'snickellane');
+define('SS_DATABASE_NAME', 'snickelstaging');
 
 define('SS_DEFAULT_ADMIN_USERNAME', 'nzd');
 define('SS_DEFAULT_ADMIN_PASSWORD', 'logmeintoaservercmsplease007*');
