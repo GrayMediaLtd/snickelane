b5eee7ff8761446410649d95261caa5761a2962a
GrayMediaLtd <jesse@graymedialtd.com>
2019-07-02 15:07:46 +1200

    edited db credentials

--- _ss_environment.php
+++ _ss_environment.php
@@ -4,8 +4,8 @@ define('SS_ENVIRONMENT_TYPE', 'live');
 
 define('SS_DATABASE_SERVER', '127.0.0.1');
 define('SS_DATABASE_USERNAME', 'forge');
-define('SS_DATABASE_PASSWORD', 'L1gUgVBIIiRDJzBcmefz');
-define('SS_DATABASE_NAME', 'snickelstaging');
+define('SS_DATABASE_PASSWORD', 'HYp6Y4NxuUZ3hyLg2jiK');
+define('SS_DATABASE_NAME', 'digital_snickel');
 
 define('SS_DEFAULT_ADMIN_USERNAME', 'nzd');
 define('SS_DEFAULT_ADMIN_PASSWORD', 'logmeintoaservercmsplease007*');
